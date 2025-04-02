ae5f5bcbc8757e8d8885fa7a5e397bce606549fb
Jonatas Joao <jojokl@hotmail.com>
2025-04-02 19:29:56 +0000

    add components

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,35 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
+            txtNome = new TextBox();
             label1 = new Label();
+            label2 = new Label();
+            txtEmail = new TextBox();
+            label3 = new Label();
+            txtIdade = new TextBox();
+            label4 = new Label();
+            txtSenha = new TextBox();
+            label5 = new Label();
+            txtConfirmarSenha = new TextBox();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
-            // textBox1
+            // txtNome
             // 
-            textBox1.Location = new Point(150, 48);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(250, 23);
-            textBox1.TabIndex = 0;
+            txtNome.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtNome.Location = new Point(150, 25);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(250, 27);
+            txtNome.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.FlatStyle = FlatStyle.Flat;
             label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 48);
+            label1.Location = new Point(12, 28);
             label1.Name = "label1";
             label1.Size = new Size(132, 21);
             label1.TabIndex = 1;
             label1.Text = "Nome Completo";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEmail.Location = new Point(150, 85);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(250, 27);
+            txtEmail.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Idade";
+            // 
+            // txtIdade
+            // 
+            txtIdade.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtIdade.Location = new Point(150, 147);
+            txtIdade.Name = "txtIdade";
+            txtIdade.Size = new Size(250, 27);
+            txtIdade.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.FlatStyle = FlatStyle.Flat;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 214);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSenha.Location = new Point(150, 211);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PasswordChar = '*';
+            txtSenha.Size = new Size(250, 27);
+            txtSenha.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(12, 270);
+            label5.Name = "label5";
+            label5.Size = new Size(131, 21);
+            label5.TabIndex = 9;
+            label5.Text = "Confirmar Senha";
+            // 
+            // txtConfirmarSenha
+            // 
+            txtConfirmarSenha.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtConfirmarSenha.Location = new Point(150, 267);
+            txtConfirmarSenha.Name = "txtConfirmarSenha";
+            txtConfirmarSenha.PasswordChar = '*';
+            txtConfirmarSenha.Size = new Size(250, 27);
+            txtConfirmarSenha.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(12, 326);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(272, 25);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "Concordar com os termos de uso";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(214, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 32);
+            button1.TabIndex = 11;
+            button1.Text = "Salvar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(312, 372);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 32);
+            button2.TabIndex = 12;
+            button2.Text = "Limpar";
+            button2.UseVisualStyleBackColor = true;
             // 
             // FormValidaCampos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(569, 363);
+            ClientSize = new Size(460, 416);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(label5);
+            Controls.Add(txtConfirmarSenha);
+            Controls.Add(label4);
+            Controls.Add(txtSenha);
+            Controls.Add(label3);
+            Controls.Add(txtIdade);
+            Controls.Add(label2);
+            Controls.Add(txtEmail);
             Controls.Add(label1);
-            Controls.Add(textBox1);
+            Controls.Add(txtNome);
+            MaximizeBox = false;
             Name = "FormValidaCampos";
             Text = "Valida Campos";
             ResumeLayout(false);
@@ -65,7 +199,18 @@
 
         #endregion
 
-        private TextBox textBox1;
+        private TextBox txtNome;
         private Label label1;
+        private Label label2;
+        private TextBox txtEmail;
+        private Label label3;
+        private TextBox txtIdade;
+        private Label label4;
+        private TextBox txtSenha;
+        private Label label5;
+        private TextBox txtConfirmarSenha;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -2,9 +2,17 @@ namespace WinFormsApp2
 {
     public partial class FormValidaCampos : Form
     {
+        private ErrorProvider _errorProvider;
         public FormValidaCampos()
         {
             InitializeComponent();
+            _errorProvider = new ErrorProvider();  
+            _errorProvider.BlinkRate = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            _errorProvider.SetError(txtConfirmarSenha, "Senha Obrigatorio");
         }
     }
 }
